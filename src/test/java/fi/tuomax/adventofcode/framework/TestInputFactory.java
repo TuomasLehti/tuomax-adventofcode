42c6663cdefac611101e7e97498dee6cbcf70f5e
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-01 01:24:40 +0300

    feat: adds a class for input handling

---
+++ src/test/java/fi/tuomax/adventofcode/framework/TestInputFactory.java
@@ -0,0 +1,33 @@
+package fi.tuomax.adventofcode.framework;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import fi.tuomax.adventofcode.HelpersForTests;
+
+public class TestInputFactory 
+extends HelpersForTests
+{
+    
+    @Test
+    public void testFromFile()
+    {
+        final String FILENAME = "/fi/tuomax/adventofcode/hello.txt";
+        final String EXPECTED = "Hello, world!";
+
+        List<String> lines;
+        try {
+            lines = InputFactory.inputFromFile(fetchResource(FILENAME));
+            assertEquals(EXPECTED, lines.get(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+}
