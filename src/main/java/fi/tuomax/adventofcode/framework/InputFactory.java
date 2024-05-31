3811479d5f078e0201eb824b8e7f67834e02c5b5
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-01 01:24:50 +0300

    tests: tests input handling

---
+++ src/main/java/fi/tuomax/adventofcode/framework/InputFactory.java
@@ -0,0 +1,30 @@
+package fi.tuomax.adventofcode.framework;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+/**
+ * <p>Manufactures input for Advent of Code puzzles from different sources.</p>
+ * 
+ * <p>Every AoC puzzle comes with its own input. This factory class 
+ * standardizes things so that the inputs can be read from local computer, over
+ * the Internet and what have you.</p>
+ * 
+ * <p>Every input I've seen so far has been text, which is why I've chosen to
+ * represent the input as a list of strings.</p>
+ */
+public class InputFactory 
+{
+
+    /**
+     * Reads input from a file.
+     */
+    public static List<String> inputFromFile(File file) 
+    throws IOException
+    {
+        return Files.readAllLines(file.toPath());
+    }
+    
+}
