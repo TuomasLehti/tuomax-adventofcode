6fdf135cb5f4c32a083f4d58d8335874f50efb6f
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-01 11:40:52 +0300

    feat: adds 2015 day 1, part 2

---
+++ src/main/java/fi/tuomax/adventofcode/y2015/d01/NotQuiteLisp_Part2.java
@@ -0,0 +1,46 @@
+package fi.tuomax.adventofcode.y2015.d01;
+
+import java.util.List;
+
+import fi.tuomax.adventofcode.framework.Parser;
+import fi.tuomax.adventofcode.framework.Solver;
+import fi.tuomax.adventofcode.framework.StringParser;
+
+public class NotQuiteLisp_Part2 
+extends Solver
+{
+
+    public NotQuiteLisp_Part2(List<String> input) 
+    {
+        super(input);
+    }
+
+    @Override
+    protected Parser manufactureParser(List<String> input) 
+    {
+        return new StringParser(input);
+    }
+
+    private StringParser getParser()
+    {
+        return (StringParser) parser;
+    }
+
+    @Override
+    protected void solve() 
+    {
+        Integer floor = 0;
+        Integer index = 0;
+        for (char c : getParser().getString().toCharArray()) {
+            if (c == '(')
+                floor++;
+            else
+                floor--;
+            if (floor.equals(-1))
+                break;
+            index++;
+        }
+        setAnswer(index);
+    }
+    
+}
