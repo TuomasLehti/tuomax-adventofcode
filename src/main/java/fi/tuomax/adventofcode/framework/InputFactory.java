6317fc8274dc6b7935b7e0a8c75f56bb09bb7ca8
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-01 13:01:28 +0300

    style: removes unnecessary import

--- src/main/java/fi/tuomax/adventofcode/framework/InputFactory.java
+++ src/main/java/fi/tuomax/adventofcode/framework/InputFactory.java
@@ -3,7 +3,6 @@ package fi.tuomax.adventofcode.framework;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
