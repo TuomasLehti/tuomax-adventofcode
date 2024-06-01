82538d44bd2474e2cecd86618dc09f7af2355f93
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-01 12:59:35 +0300

    feat: adds possibility to read tests from json

---
+++ src/test/java/fi/tuomax/adventofcode/PuzzleTestCase.java
@@ -0,0 +1,8 @@
+package fi.tuomax.adventofcode;
+
+import java.util.List;
+
+public record PuzzleTestCase(
+    List<String> input,
+    String expectedAnswer
+) {}
--- src/test/java/fi/tuomax/adventofcode/PuzzleTester.java
+++ src/test/java/fi/tuomax/adventofcode/PuzzleTester.java
@@ -1,9 +1,20 @@
 package fi.tuomax.adventofcode;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import fi.tuomax.adventofcode.framework.InputFactory;
+import fi.tuomax.adventofcode.framework.Metadata;
 
 /**
  * Includes helpers for testing. This class should be inherited by all other
@@ -11,6 +22,39 @@ import java.net.URL;
  */
 public class PuzzleTester 
 {
+
+    protected JSONObject json;
+
+    /**
+     * Reads the json file which contains the test cases.
+     */
+    protected void readJson(Metadata metadata)
+    {
+        File file = new File(InputFactory.inputLocation(metadata, "tests.json"));
+        try {
+            String content = new String(Files.readAllBytes(file.toPath()));
+            json = new JSONObject(content);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Fetches the suite of tests from json.
+     */
+    protected List<PuzzleTestCase> fetchTestCases(Metadata metadata)
+    {
+        readJson(metadata);
+        JSONArray suite = json.getJSONArray(String.format("part%d", metadata.part()));
+
+        List<PuzzleTestCase> tests = new ArrayList<>();
+        for (Object o : suite) {
+            JSONObject test = (JSONObject) o;
+            tests.add(new PuzzleTestCase(InputFactory.inputFromString(test.getString("input")), test.getString("answer")));
+        }
+        return tests;
+    }
+
 
     public File fetchResource(String filename)
     {
