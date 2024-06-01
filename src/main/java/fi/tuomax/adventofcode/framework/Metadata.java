9cb3a11d0fb3ff53cd433216eb32dd9ee491666e
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-01 12:06:39 +0300

    feat: adds metadata

---
+++ src/main/java/fi/tuomax/adventofcode/framework/Metadata.java
@@ -0,0 +1,37 @@
+package fi.tuomax.adventofcode.framework;
+
+/**
+ * <p>The metadata associated with <a href="https://adventofcode.com/">Advent 
+ * of Code</a> puzzles.</p>
+ * 
+ * <p>Advent of Code is a yearly programming event. There are 25 programming
+ * puzzles in each year's event, one for each day in december before christmas, 
+ * and each puzzle is divided into two parts. This means that at least the 
+ * year, day and part are needed to tell different puzzles apart.</p>
+ * 
+ * <p>Each day has a name, which is included here for pretty printouts of the
+ * results of a puzzle.</p>
+ * 
+ * <p>Sometimes it is possible to use different algorithms to solve a puzzle.
+ * The version-field tells apart the different versions of algorightm.</p>
+ * 
+ * @param year
+ *      The year of the event.
+ * @param day
+ *      The day in the current event.
+ * @param part
+ *      The part of the current puzzle.
+ * @param name
+ *      The name of the puzzle.
+ * @param version
+ *      The version of the solving algorithm.
+ */
+public record Metadata(
+    Integer year,
+    Integer day,
+    Integer part,
+    String name,
+    String version
+) 
+{
+}
--- src/main/java/fi/tuomax/adventofcode/framework/Solver.java
+++ src/main/java/fi/tuomax/adventofcode/framework/Solver.java
@@ -5,16 +5,33 @@ import java.util.List;
 public abstract class Solver 
 {
 
+    private Metadata metadata = null;
+
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
     protected Parser parser;
 
-    protected String answer = "";
+    private String answer = "";
+
+    public String getAnswer() {
+        return answer;
+    }
 
     private Stopwatch stopwatch = new Stopwatch();
     
+    public Stopwatch getStopwatch() {
+        return stopwatch;
+    }
+
     public Solver(List<String> input)
     {
         this.parser = manufactureParser(input);
+        setMetadata();
     }
+
+    protected abstract void setMetadata();
 
     protected abstract Parser manufactureParser(List<String> input);
 
