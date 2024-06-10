ea263af18c0634764e6a6ce36f7dff5cf1f2658e
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-10 16:57:27 +0300

    refactor: runners are now inherited from base class

--- src/main/java/fi/tuomax/adventofcode/framework/running/LatestRunner.java
+++ src/main/java/fi/tuomax/adventofcode/framework/running/LatestRunner.java
@@ -3,22 +3,18 @@ package fi.tuomax.adventofcode.framework.running;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import fi.tuomax.adventofcode.framework.solving.Metadata;
 import fi.tuomax.adventofcode.framework.solving.Solver;
 import fi.tuomax.adventofcode.framework.storing.Algorithm;
 import fi.tuomax.adventofcode.framework.storing.Day;
 import fi.tuomax.adventofcode.framework.storing.Year;
 
-public class LatestRunner 
+public class LatestRunner
+extends Runner
 {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
-    public List<List<Solver>> run(Metadata metadata, Year year)
-    {
+    @Override
+    protected List<List<Solver>> collectSolvers(Metadata metadata, Year year) {
         List<List<Solver>> runned = new ArrayList<>();
 
         if (!year.containsDay(metadata.day()))
@@ -34,26 +30,13 @@ public class LatestRunner
 
         List<Solver> solvers = new ArrayList<>();
         if (algo.containsPart(1)) {
-            run(algo.getPart(1));
             solvers.add(algo.getPart(1));
         }
         if (algo.containsPart(2)) {
-            run(algo.getPart(2));
             solvers.add(algo.getPart(2));
         }
         runned.add(solvers);
         return runned;
     }
-
-    private void run(Solver solver)
-    {
-        logger.info(String.format(
-            "Running AoC %d, day %d, part %d: %s", 
-            solver.getMetadata().year(), 
-            solver.getMetadata().day(), 
-            solver.getMetadata().part(), 
-            solver.getMetadata().name()));
-        solver.run();
-}
     
 }
--- src/main/java/fi/tuomax/adventofcode/framework/running/YearRunner.java
+++ src/main/java/fi/tuomax/adventofcode/framework/running/YearRunner.java
@@ -3,21 +3,17 @@ package fi.tuomax.adventofcode.framework.running;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import fi.tuomax.adventofcode.framework.solving.Metadata;
 import fi.tuomax.adventofcode.framework.solving.Solver;
 import fi.tuomax.adventofcode.framework.storing.Algorithm;
 import fi.tuomax.adventofcode.framework.storing.Year;
 
-public class YearRunner 
+public class YearRunner
+extends Runner
 {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
-    public List<List<Solver>> run(Metadata metadata, Year year)
-    {
+    @Override
+    protected List<List<Solver>> collectSolvers(Metadata metadata, Year year) {
         List<List<Solver>> runned = new ArrayList<>();
 
         for (Integer dayNo : year.getDayNos()) {
@@ -28,11 +24,9 @@ public class YearRunner
 
             List<Solver> solvers = new ArrayList<>();
             if (algo.containsPart(1)) {
-                run(algo.getPart(1));
                 solvers.add(algo.getPart(1));
             }
             if (algo.containsPart(2)) {
-                run(algo.getPart(2));
                 solvers.add(algo.getPart(2));
             }
             runned.add(solvers);
@@ -40,17 +34,5 @@ public class YearRunner
 
         return runned;
     }
-
-    private void run(Solver solver)
-    {
-        logger.info(String.format(
-            "Running AoC %d, day %d, part %d: %s", 
-            solver.getMetadata().year(), 
-            solver.getMetadata().day(), 
-            solver.getMetadata().part(), 
-            solver.getMetadata().name()));
-        solver.run();
-    }
-
     
 }
