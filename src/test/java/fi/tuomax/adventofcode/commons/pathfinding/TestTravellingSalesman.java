85035e0208ebb1f1941751e4252c431fd4a5386f
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-06-15 21:59:11 +0300

    feat: adds an implementation of a solution to the travelling salesman problem

---
+++ src/test/java/fi/tuomax/adventofcode/commons/pathfinding/TestTravellingSalesman.java
@@ -0,0 +1,34 @@
+package fi.tuomax.adventofcode.commons.pathfinding;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestTravellingSalesman 
+{
+
+    @Test
+    public void test()
+    {
+        Graph graph = new Graph();
+
+        graph.addNodesAndUndirectedEdge("Helsinki",  "Pori", 237);
+        graph.addNodesAndUndirectedEdge("Pori",  "Helsinki", 237);
+        graph.addNodesAndUndirectedEdge("Helsinki",  "Tampere", 179);
+        graph.addNodesAndUndirectedEdge("Tampere",  "Helsinki", 179);
+        graph.addNodesAndUndirectedEdge("Pori",  "Tampere", 109);
+        graph.addNodesAndUndirectedEdge("Tampere",  "Pori", 109);
+
+        // Hki - Pri - Tre 237 + 109 = 346
+        // Hki - Tre - Pri 179 + 109 = 288
+        // Pri - Tre - Hki 109 + 179 = 288
+        // Pri - Hki - Tre 237 + 179 = 416
+        // Tre - Hki - Pri 179 + 237 = 416
+        // Tre - Pri - Hki 109 + 237 = 346
+
+        assertEquals(288, (int) TravellingSalesmanBruteforce.minDistance(graph));
+        assertEquals(416, (int) TravellingSalesmanBruteforce.maxDistance(graph));
+
+    }
+
+}
