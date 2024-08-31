4eb0ee82b562db744d3fecb22837695852749211
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-08-31 13:34:50 +0300

    build: adds necessary files for 2022 day 5

---
+++ src/main/java/fi/tuomax/adventofcode/year2022/day05/SupplyStacks_Parser.java
@@ -0,0 +1,21 @@
+package fi.tuomax.adventofcode.year2022.day05;
+
+import java.util.List;
+
+import fi.tuomax.adventofcode.framework.parsing.Parser;
+
+public class SupplyStacks_Parser 
+extends Parser
+{
+
+    public SupplyStacks_Parser(List<String> input) 
+    {
+        super(input);
+    }
+
+    @Override
+    public void parse() 
+    {
+    }
+    
+}
---
+++ src/main/java/fi/tuomax/adventofcode/year2022/day05/SupplyStacks_Part1.java
@@ -0,0 +1,40 @@
+package fi.tuomax.adventofcode.year2022.day05;
+
+import fi.tuomax.adventofcode.framework.solving.Metadata;
+import fi.tuomax.adventofcode.framework.parsing.Parser;
+import fi.tuomax.adventofcode.framework.solving.Solver;
+import java.util.List;
+
+/**
+ * <p>Solves Advent of Code 2022, day 5, part 1:
+ * Supply Stacks.</p>
+ *
+ * @see
+ *      <a href="https://adventofcode.com/2022/day/5">
+ *      Puzzle on the Advent of Code website.</a></p>
+ */
+public class SupplyStacks_Part1
+extends Solver
+{
+
+    @Override
+    protected Metadata manufactureMetadata()
+    {
+        return new Metadata(
+            2022, 5, 1,
+            "Supply Stacks", ""
+        );
+    }
+
+    @Override
+    protected Parser manufactureParser(List<String> input)
+    {
+        return null;
+    }
+
+    @Override
+    protected void solve()
+    {
+    }
+
+}
