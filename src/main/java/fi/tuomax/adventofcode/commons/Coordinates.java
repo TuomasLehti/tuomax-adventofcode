e1e3f1da0edb7d4f8a8ebaf8cc603f4e5f660d38
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-11-17 12:04:30 +0200

    feat: adds rotation of coordinates

--- src/main/java/fi/tuomax/adventofcode/commons/Coordinates.java
+++ src/main/java/fi/tuomax/adventofcode/commons/Coordinates.java
@@ -1,5 +1,10 @@
 package fi.tuomax.adventofcode.commons;
 
+import fi.tuomax.adventofcode.commons.Direction.TurnDirection;
+
+/**
+ * TODO: docs
+ */
 public record Coordinates(Long x, Long y) 
 implements Comparable<Coordinates>
 {
@@ -14,6 +19,38 @@ implements Comparable<Coordinates>
     public Coordinates translate(Coordinates amount) 
     {
         return new Coordinates(this.x + amount.x, this.y + amount.y);
+    }
+
+    /** Supports only 90 degree rotations because of integer coordinate system. */
+    public Coordinates rotate90Degrees(TurnDirection direction)
+    {
+
+        /* Matrix multiplication time!!!
+         * 
+         * [ a b ] [ x ]  = [ ax + by ]
+         * [ c d ] [ y ]    [ cx + dy ]
+         * 
+         * counter-clockwise
+         * 
+         * [ 0 -1 ] [ 5 ] = [ 0*5 - 1*2 ] = [ 0 - 2 ] = [ -2 ]
+         * [ 1  0 ] [ 2 ]   [ 1*5 + 0*2 ]   [ 5 + 0 ]   [  5 ]
+         * 
+         * x' = -y
+         * y' = x
+         * 
+         * clockwise
+         * 
+         * [  0  1 ] [ x ]
+         * [ -1  0 ] [ y ]
+         * 
+         * x' = 0x + 1y
+         * y' = -1x + 0y
+         * 
+         */
+        return direction == TurnDirection.LEFT ? 
+            new Coordinates(this.y * -1, this.x) :
+            new Coordinates(this.y, this.x * -1);
+        
     }
 
     public Integer xAsInt() 
--- src/test/java/fi/tuomax/adventofcode/commons/TestCoordinates.java
+++ src/test/java/fi/tuomax/adventofcode/commons/TestCoordinates.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import fi.tuomax.adventofcode.commons.Direction.TurnDirection;
+
 public class TestCoordinates 
 {
 
@@ -15,6 +17,30 @@ public class TestCoordinates
         assertEquals(10L, (long) from.taxicabDistance(new Coordinates(45L, 55L)));
         assertEquals(10L, (long) from.taxicabDistance(new Coordinates(55L, 45L)));
         assertEquals(10L, (long) from.taxicabDistance(new Coordinates(45L, 45L)));
+    }
+
+    @Test
+    public void Test_Rotate90Degrees()
+    {
+        Coordinates coords = Coordinates.fromInteger(2, 0);
+        coords = coords.rotate90Degrees(TurnDirection.RIGHT);
+        assertEquals(coords, Coordinates.fromInteger(0, -2));
+        coords = coords.rotate90Degrees(TurnDirection.RIGHT);
+        assertEquals(coords, Coordinates.fromInteger(-2, 0));
+        coords = coords.rotate90Degrees(TurnDirection.RIGHT);
+        assertEquals(coords, Coordinates.fromInteger(0, 2));
+        coords = coords.rotate90Degrees(TurnDirection.RIGHT);
+        assertEquals(coords, Coordinates.fromInteger(2, 0));
+
+        coords = Coordinates.fromInteger(10, 4);
+        coords = coords.rotate90Degrees(TurnDirection.LEFT);
+        assertEquals(coords, Coordinates.fromInteger(-4, 10));
+        coords = coords.rotate90Degrees(TurnDirection.LEFT);
+        assertEquals(coords, Coordinates.fromInteger(-10, -4));
+        coords = coords.rotate90Degrees(TurnDirection.LEFT);
+        assertEquals(coords, Coordinates.fromInteger(4, -10));
+        coords = coords.rotate90Degrees(TurnDirection.LEFT);
+        assertEquals(coords, Coordinates.fromInteger(10, 4));
     }
     
 }
