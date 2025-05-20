5d1f90368e92b90780f2f8480874b515bb30b684
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-05-20 21:17:46 +0300

    refactor: the registers should now be given from the solver

--- src/main/java/fi/tuomax/adventofcode/commons/cpu/Cpu_Parser.java
+++ src/main/java/fi/tuomax/adventofcode/commons/cpu/Cpu_Parser.java
@@ -6,26 +6,50 @@ import java.util.List;
 
 import fi.tuomax.adventofcode.framework.parsing.Parser;
 
+/**
+ * Parses a list of instructions and programs a cpu.
+ */
 public class Cpu_Parser 
 extends Parser
 {
 
-    private Cpu cpu = new Cpu(new HashSet<String>(
-            Arrays.asList(new String[]{"a", "b", "c", "d"})));
+    private Cpu cpu = null;
 
     private InstructionFactory factory;
 
+    /**
+     * Getter for the cpu.
+     * @return
+     *      The cpu at its initial state with the program.
+     */
     public Cpu getCpu() 
     {
         return cpu;
     }
 
-    public Cpu_Parser(List<String> input, InstructionFactory factory) 
-    {
+    /**
+     * Constructs a Cpu parser.
+     * @param input
+     *      The input from Advent of Code.
+     * @param factory
+     *      The factory responsible for parsing the string of text to an 
+     *      instruction.
+     * @param registers
+     *      The names of the registers this cpu should have.
+     */
+    public Cpu_Parser(
+        List<String> input, 
+        InstructionFactory factory, 
+        String[] registers
+    ) {
         super(input);
         this.factory = factory;
+        cpu = new Cpu(new HashSet<String>(Arrays.asList(registers)));
     }
 
+    /**
+     * Parses the input.
+     */
     @Override
     public void parse() 
     {
